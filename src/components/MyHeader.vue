5c06faf259c3ccf9c3c3a9e530d2cbc3c1e15921
MfsTeller <i1211435.kosen@gmail.com>
2022-08-07 20:55:30 +0900

    improve header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -3,16 +3,22 @@
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark flex-md-nowrap p-0 shadow">
       <div class="container-fluid">
         <a class="navbar-brand" href="index.html">Time Tracker GUI</a>
-        <div class="dropdown collapse navbar-collapse" id="navbarSupportedContent">
-          <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            Development
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" target="_blank" href="https://v3.ja.vuejs.org/guide/introduction.html">Vue.js</a>
-            <a class="dropdown-item" target="_blank"
-              href="https://getbootstrap.jp/docs/5.0/getting-started/introduction/">Bootstrap</a>
-          </div>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Development
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" target="_blank"
+                  href="https://v3.ja.vuejs.org/guide/introduction.html">Vue.js</a>
+                <a class="dropdown-item" target="_blank"
+                  href="https://getbootstrap.jp/docs/5.0/getting-started/introduction/">Bootstrap</a>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
